9dc786017032f426314419ef714d3b9169e85472
IsabellaG08 <isabellagore08@gmail.com>
2024-10-07 18:31:47 +0000

    All done

--- predicates.py
+++ predicates.py
@@ -6,42 +6,57 @@ import pytest
 
 # is x between a and b
 def between(x, a, b):
-  pass
-  return True
+  if x >= a and x <= b:
+
+    return True
+  else:
+    return False
   
 # is x divisibleBy a ?
 def isDivisibleBy(x, a):
-  
-  return False
+  if x%a==0:
+    return True
+  else:
+    return False
   
 # is x even?
 def isEvenNumber(x):
-  return False
+  if x%2==0:
+    return True
+  else:
+    return False
   
 # is x odd?
 def isOddNumber(x):
-  return False
+  if x%2==1:
+    return True
+  else:
+    return False
   
 # this is done
 def isInteger(x):
-  return False  # no change needed here
+  if type(x) == int:
+    return True
+  else:
+    return False  # no change needed here
 
 # withinRadius is the point (x1,y1) within distance of the point (x2, y2)
 def withinRadius(x1, y1, x2, y2, distance):
-  # t1.xcor()   t2.xcor()
-  # t1.ycor()   t2.ycor()
-  return False
+  if math.sqrt((x1-x2)**2+ (y1-y2)**2) <= distance:
+    return True
+  else:
+    return False
   
 # do the sides a, b, c make a Pythagorean triangle
 # remember the Pythagorean theorem
 def isPythagorean(a, b, c):
-  return False
+  if a**2+b**2==c**2:
+    return True
+  else:
+    return False
   
-# is number a prime number
-# I suggest using a loop here
-def isPrime(number):
-  
-  return True
+
+
   
 # is (x, y) a point with in the circle defined by (cx, cy) and radius
 # think about the circle equation (x-h)^2 + (y-k) ^2 = r^2
